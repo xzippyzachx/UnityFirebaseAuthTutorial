42813ac682584234d7f6d3a8de04a89f46479511
Zach <zacharyross1234@gmail.com>
2020-08-20 19:45:16 -0400

    Add initial starter project

---
+++ FirebaseAuthTutorialStarterProject/Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject loginUI;
+    public GameObject registerUI;
+
+
+
+
+    //Functions to change the login screen UI
+    public void LoginScreen() //Back Btn
+    {
+        loginUI.SetActive(true);
+        registerUI.SetActive(false);
+    }
+    public void RegisterScreen() // Regester Btn
+    {
+        loginUI.SetActive(false);
+        registerUI.SetActive(true);
+    }
+
+
+}
